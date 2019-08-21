8026d044cc896562ab69320ade7c3737fc5922dc
Jonas Kibildis <kibildis@merann.ru>
2019-08-21 13:22:54 +0300

    pid selection added

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,27 @@
 import * as vscode from 'vscode';
 const child_process = require('child_process');
 
-
 export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(vscode.commands.registerCommand('ReadPid', readPid));
   async function readPid() {
     const asd = vscode.workspace.getConfiguration().get('ReadPid.AppName');
 
-    let pid = child_process.execSync(`pidof ${asd}`);
-    vscode.window.showInformationMessage(`pidof ${asd} = ` + pid.toString());
-    return String(pid);
+    let pid = child_process.execSync(`pidof ${asd} ;`);
+    var str = pid.toString();
+    vscode.window.showInformationMessage(`pidof ${asd} = ` + str);
+    var pids = str.split(' ');
+    var selectedValue = "";
+    if (pids.length > 1) {
+      await vscode.window.showQuickPick(pids).then(value => {
+          if (!value) return;
+          selectedValue = value;
+      });
+      return selectedValue;
+    }
+    else {
+      return str;
+    }
   }
 }
 
